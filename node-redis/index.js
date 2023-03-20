769d0d4ff3ec80660b789ebf92c7282c89e44cd7
Office VMware <officevm@mail.com>
2023-03-20 17:58:32 +0600

    Added

--- node-redis/index.js
+++ node-redis/index.js
@@ -1,16 +1,3 @@
-// const redis = require('redis')
-
-// const client = redis.createClient({
-//     url: 'redis://redis:6379'
-//   })
-
-// await client.connect()
-
-// client.set("name", "Flavio")
-// client.set("age", 37)
-
-// const value = await client.get("name")
-
 const redis = require('redis')
 
 const client = redis.createClient({
