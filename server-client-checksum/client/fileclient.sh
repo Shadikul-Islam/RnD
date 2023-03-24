2066a109fcf68c8136de640fe7a9db996f2b4da1
Shadikul Islam <sadikul.islam@gmail.com>
2023-03-25 00:19:43 +0600

    File Added

--- server-client-checksum/client/fileclient.sh
+++ server-client-checksum/client/fileclient.sh
@@ -5,4 +5,4 @@ docker volume create clientvol
 
 # build and run the client container
 docker build -t client-image .
-docker run -it --name client-container --network my-network -v clientvol:/clientdata client-image
+docker run -d --name client-container --network my-network -v clientvol:/clientdata client-image
--- server-client-checksum/server/fileserver.sh
+++ server-client-checksum/server/fileserver.sh
@@ -8,4 +8,4 @@ docker network create my-network
 
 # build and run the server container
 docker build -t server-image .
-docker run -it --name server-container --network my-network -v servervol:/serverdata -p 3000:3000 server-image
+docker run -d --name server-container --network my-network -v servervol:/serverdata -p 3000:3000 server-image
