fe4716a17886df7e805a1481296fe3aac9163a13
Shadikul Islam <officevm@mail.com>
2023-04-03 10:48:07 +0600

    Sonarqube project added

---
+++ sonarqube/init_sonar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download the SonarQube Community Branch Plugin
+curl -L -o /opt/sonarqube/extensions/plugins/sonarqube-community-branch-plugin-1.9.0.jar https://github.com/mc1arke/sonarqube-community-branch-plugin/releases/download/1.9.0/sonarqube-community-branch-plugin-1.9.0.jar
+
+# Run SonarQube
+exec /opt/sonarqube/bin/sonar.sh run
