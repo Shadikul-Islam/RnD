ef4eabfa6d19a582a2b32727f8d8d3dd3d780825
Shadikul Islam <sadikul.islam@gmail.com>
2023-05-14 02:18:00 +0600

    Project Added

--- celestia setup & monitoring/celestiaLight/setup-script-2.sh
+++ celestia setup & monitoring/celestiaLight/setup-script-2.sh
@@ -1,17 +1,7 @@
-# Go to Temp Folder
-cd /tmp
-
-# Download Script to Setup Celestia Light Node for Ubuntu
-wget https://raw.githubusercontent.com/GLCNI/celestia-node-scripts/main/multi-network/light-node.sh
-
-# Make Executable
-chmod a+x light-node.sh
-
-# Run Script
-./light-node.sh
-
 # Stop Celestia Service
 sudo systemctl stop celestia-light
+
+# Remove the Previous Service File
 sudo rm /etc/systemd/system/celestia-light.service
 
 # Create a System Service for Celestia
